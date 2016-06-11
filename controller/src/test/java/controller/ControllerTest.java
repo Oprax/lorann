00e3e079db1bc3d911534e6dad941bbcdb148b95
Oprax <oprax@me.com>
2016-06-11 18:58:50 +0200

    Test

---
+++ controller/src/test/java/controller/ControllerTest.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+    @Test
+    public void control() throws Exception {
+
+    }
+
+    @Test
+    public void orderPerform() throws Exception {
+
+    }
+
+}

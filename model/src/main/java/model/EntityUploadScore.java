5114abe8e5c1008f6f1943d5bc196c175fc39459
okiz <qstrinati@gmail.com>
2016-06-18 16:55:10 +0200

    deleted useless functions and files

--- model/src/main/java/model/EntityUploadScore.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-/**
- * Created by Yog on 16/06/2016.
- */
-abstract public class EntityUploadScore {
-}
--- model/src/main/java/model/GetHighscore.java
+++ model/src/main/java/model/GetHighscore.java
@@ -6,23 +6,11 @@ package model;
  * @author cyril SNIADACH
  */
 class GetHighscore extends EntityScore {
-
-    private int	score;
     /**
      * Instantiates a new Get Highscore.
      */
     public GetHighscore() {
 
-    }
-
-    public void setGetHighScore(final int score)
-    {
-        this.score = score;
-    }
-
-    public int getGetHighScore()
-    {
-        return this.score;
     }
 }
 

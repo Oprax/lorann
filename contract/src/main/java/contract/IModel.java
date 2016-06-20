a976852c077ad74cff79e6574cd568a063f19eb0
QuipeDorian <dorian.retter@viacesi.fr>
2016-06-20 12:09:14 +0200

    Java Doc Contract

--- contract/src/main/java/contract/IModel.java
+++ contract/src/main/java/contract/IModel.java
@@ -18,8 +18,6 @@ public interface IModel {
 	 */
 	Observable getObservable();
 
-
-
 	/**
 	 * Gets the map content.
 	 * Load the text file of the map needed as it appear in the DataBase
@@ -30,16 +28,16 @@ public interface IModel {
 
 	/**
 	 * Load the map using the key in the table map of the database
-	 * @param key
+	 * @param key Id in the DB
 	 * @author Dorian
 	 */
 	void loadMap(String key);
 
 	/**
-	 * @param c
+	 * @param c c for character on the text file
 	 * @param pos
 	 * Use the parser to locate, on the map, the position for the mobile elements, and the door, in order to make them move when needed
-	 * @return
+	 * @return map
 	 */
 	IElement element(char c, Point pos);
 
@@ -51,8 +49,8 @@ public interface IModel {
     String[][] getHighScore();
 
     /**
-     * @param score
-     * @param nickname
+     * @param score int made by user
+     * @param nickname string typed
      * At win or loose POPUP the actual score made is load in the DataBase
      * @author Dorian
      */
